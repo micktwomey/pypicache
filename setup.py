aa360c4b5f1ae2dbdbb18992a352cd95ed7719fa
Matthew Schinckel <matt@schinckel.net>
2013-04-09 20:21:20 +0930

    Use setuptools.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pypicache',
@@ -13,5 +13,15 @@ setup(
             'static/*/*',
             'templates/*.html',
         ]
-    }
+    },
+    install_requires=[
+        'certifi==0.0.8',
+        'chardet==2.1.1',
+        'distribute==0.6.34',
+        'Flask==0.9',
+        'Jinja2==2.6',
+        'requests==1.1.0',
+        'Werkzeug==0.8.3',
+    ]
+    
 )
